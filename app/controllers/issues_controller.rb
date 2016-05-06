89daf776512aed25d3891fb83767ea1a3cd5cec1
xiao1994 <928431564@qq.com>
2016-05-06 11:58:15 +0800

    16.5.6

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,43 @@
+class IssuesController < ApplicationController
+  def show
+    @issue = Issue.find(params[:id])
+    @comments = @issue.comments
+  end
+
+  def new
+    if not current_user
+      flash[:notice] = "没有执行此操作的权限，请先登录"
+      redirect_to :root
+      return
+    else
+      @issue = Issue.new
+    end
+  end
+
+  def edit
+    @issue = Issue.find(params[:id])
+  end
+
+  def create
+    Issue.create(issue_params)
+    redirect_to :root
+  end
+
+  def update
+    i = Issue.find(params[:id])
+    i.update_attributes(issue_params)
+    redirect_to :root
+  end
+
+  def destroy
+    i = Issue.find(params[:id])
+    i.destroy
+    redirect_to :root
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:title, :content, :user_id)
+  end
+end
---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,6 @@
+class PageController < ApplicationController
+  def welcome
+    @issues  = Issue.all.reverse
+  end
+
+end
